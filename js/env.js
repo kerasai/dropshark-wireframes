a9f9ca18d184b5c5247f4a806c033dd49c31accd
Will Long <will@kerasai.com>
2015-11-03 07:19:49 -0600

    added stats to env page

--- js/env.js
+++ js/env.js
@@ -3,14 +3,17 @@
     google.setOnLoadCallback(drawCharts);
 
     function drawCharts() {
-        drawOpcacheMemory();
+        drawRamChart();
+        drawDiskChart();
+        drawLoadChart();
     }
 
-    function drawOpcacheMemory() {
+    function drawRamChart() {
         var data = google.visualization.arrayToDataTable([
             ['Type', 'Allocation'],
-            ['Used', 57],
-            ['Free', 39],
+            ['Used', 878],
+            ['Free', 1123],
+            ['Cached', 209],
         ]);
 
         var options = {
@@ -19,8 +22,45 @@
             pieSliceText: 'none',
         };
 
-        var element = $('#opcache-memory-chart')[0];
+        var element = $('#ram-chart')[0];
         var chart = new google.visualization.PieChart(element);
+
+        chart.draw(data, options);
+    }
+
+    function drawDiskChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Type', 'Allocation'],
+            ['Free', 33.11],
+            ['Used', 2.29],
+        ]);
+
+        var options = {
+            pieHole: 0.4,
+            legend: 'none',
+            pieSliceText: 'none',
+        };
+
+        var element = $('#disk-chart')[0];
+        var chart = new google.visualization.PieChart(element);
+
+        chart.draw(data, options);
+    }
+
+    function drawLoadChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Type', 'Allocation'],
+            ['1 min', .06],
+            ['5 min', .11],
+            ['15 min', .09],
+        ]);
+
+        var options = {
+            legend: 'none',
+        };
+
+        var element = $('#load-chart')[0];
+        var chart = new google.visualization.ColumnChart(element);
 
         chart.draw(data, options);
     }
