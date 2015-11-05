5250411fd7dfdc400899147324fc05155ead95b2
Will Long <will@kerasai.com>
2015-11-05 11:09:54 -0600

    added content page

---
+++ js/content.js
@@ -0,0 +1,62 @@
+(function ($) {
+
+    var colors = {
+        general: ['#0cab41', '#0c76ab', '#ab0c76', '#ab410c'],
+        status: ['#cc060c', '#f2930c', '#0cab41'],
+        usage: ['#cc060c', '#0cab41'],
+    };
+
+    google.setOnLoadCallback(drawCharts);
+
+    function drawCharts() {
+        drawContentTypeChart();
+        drawContentRoleChart();
+    }
+
+    function drawContentTypeChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Date', 'Article', 'Event', 'Page'],
+            ['9/3', 422, 128, 3],
+            ['9/10', 464, 139, 8],
+            ['9/17', 498, 145, 10],
+            ['9/24', 545, 154, 14],
+            ['10/1', 607, 188, 14],
+            ['10/8', 734, 222, 15],
+            ['10/15', 812, 237, 17],
+        ]);
+
+        var options = {
+            legend: 'bottom',
+            isStacked: true,
+            colors: colors.general,
+        };
+
+        var element = $('#content-type-chart')[0];
+        var chart = new google.visualization.AreaChart(element);
+
+        chart.draw(data, options);
+    }
+
+
+    function drawContentRoleChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Date', 'Anonymous', 'Authenticated', 'Administrator', 'Editor'],
+            ['9/17', 50, 725, 55, 375],
+            ['9/24', 55, 750, 60, 450],
+            ['10/1', 60, 800, 65, 550],
+            ['10/8', 82, 900, 70, 625],
+            ['10/15', 84, 982, 76, 714],
+        ]);
+
+        var options = {
+            legend: 'bottom',
+            colors: colors.general,
+        };
+
+        var element = $('#content-role-chart')[0];
+        var chart = new google.visualization.ColumnChart(element);
+
+        chart.draw(data, options);
+    }
+
+})(jQuery);
